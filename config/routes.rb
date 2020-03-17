29f045cda844f6949c0fd3fed0885eadc980648c
firmo holanda <firmo@viize.com>
2020-03-17 14:34:32 -0300

    create layouts views for header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   
-  get 'static_pages/home'
-  get 'static_pages/about'
   root 'static_pages#home'
 
   # users
   resources :users
+
+  # static_pages
+  resources :static_pages
+
+  # session routes
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
 
 end
