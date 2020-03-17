d8ec75cadedcb54d1c9951d1844b6624d02ea1ac
firmo holanda <firmo@viize.com>
2020-03-17 13:38:20 -0300

    add static_pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
   
   def new
     @user = User.new
