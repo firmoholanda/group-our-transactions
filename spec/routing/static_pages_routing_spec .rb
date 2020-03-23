55f764df1bc4d9196764bbac7342aca1ef93b6b8
firmo holanda <firmo@viize.com>
2020-03-23 20:39:29 -0300

    fix rubocop errors

--- spec/routing/static_pages_routing_spec .rb
+++ spec/routing/static_pages_routing_spec .rb
@@ -5,7 +5,6 @@ RSpec.describe Static_pagesController, type: :routing do
     it 'routes to #home' do
       expect(get: '/home').to route_to('static_pages#home')
     end
-
     it 'routes to #about' do
       expect(get: '/about').to route_to('static_pages#about')
     end
