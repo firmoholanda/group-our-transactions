bfaad394ae581dad01d22d86a062a7cf8a580948
firmo holanda <firmo@viize.com>
2020-03-23 21:15:06 -0300

    update new line

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
   private
 
   def downcase_email
